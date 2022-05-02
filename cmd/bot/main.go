5145826b74985d15c9357889552f8ce558f20c40
pull_ya <pull_ya@mail.ru>
2022-05-02 23:18:39 +0300

    HELP command

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -6,8 +6,10 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+const token = "463334009:AAEHiO4nel30tyNEKgNMhZtqzn5HnoZHHHU"
+
 func main() {
-	bot, err := tgbotapi.NewBotAPI("463334009:AAEHiO4nel30tyNEKgNMhZtqzn5HnoZHHHU")
+	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
 		log.Panic(err)
 	}
@@ -28,8 +30,13 @@ func main() {
 		if update.Message != nil { // If we got a message
 			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
+			if update.Message.Command() == "help" {
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "HELP command manual")
+				bot.Send(msg)
+				continue
+			}
+
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "You sent: "+update.Message.Text)
-			//			msg.ReplyToMessageID = update.Message.MessageID
 
 			bot.Send(msg)
 		}
